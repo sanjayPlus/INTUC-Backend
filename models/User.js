90927acec471e3e6c2f0394d596159122794e870
Sanjay9745 <sanjaysanthosh919@gmail.com>
2024-01-03 14:32:20 +0530

    password removed from details

--- models/User.js
+++ models/User.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     type: String,
   },
   date_of_birth:{
-    type:String,
+    type:Date,
   },
   block:String,
   constituency:String,
@@ -75,6 +75,24 @@ const userSchema = new mongoose.Schema({
     merchantTransactionId:String,
   }]
 });
+
+// Virtual for age calculation based on date_of_birth
+userSchema.virtual('age').get(function() {
+  if (this.date_of_birth) {
+    const today = new Date();
+    const birthDate = new Date(this.date_of_birth);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+  return null; // Return null if date_of_birth is not set
+});
+
+// Setting the virtual field to appear in JSON
+userSchema.set('toJSON', { getters: true });
 
 const User = mongoose.model('User', userSchema);
 
